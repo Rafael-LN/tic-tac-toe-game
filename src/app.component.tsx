41fedcf38ca4b1d969540a5cd075eb2ff016d398
Rafael_LN <39883801+Rafael-LN@users.noreply.github.com>
2021-09-07 23:49:49 +0100

    Updated import after tsconfig changes

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header/header.component';
+import Header from 'components/Header/header.component';
 import Footer from 'components/Footer/footer.component';
 import User from 'components/User/user.component';
 
