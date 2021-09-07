457334b09312c0b70a105cf786d45f40bfc4846d
Rafael_LN <39883801+Rafael-LN@users.noreply.github.com>
2021-09-07 23:39:23 +0100

    Added User to App

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,10 +1,12 @@
-import Footer from 'components/footer.component';
 import Header from './components/header.component';
+import Footer from 'components/footer.component';
+import User from 'components/user.component';
 
 function App() {
   return (
     <>
       <Header/>
+      <User/>
       <div>SwordHealth</div>
       <Footer/>
     </>
