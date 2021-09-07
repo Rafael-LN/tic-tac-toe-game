9eb83e321574f38b750fc825ca20a180289c3a86
Rafael_LN <39883801+Rafael-LN@users.noreply.github.com>
2021-09-07 23:38:46 +0100

    Created User component and style

---
+++ src/components/user.component.tsx
@@ -0,0 +1,32 @@
+import { Col, Row } from "react-flexbox-grid"
+import { ReactComponent as Photo } from 'assets/images/photo.svg';
+import { UserContainer, UserDetails } from "./user.style";
+
+const User = () => {
+    return (
+        <UserContainer>
+            <Row>
+                <Col xs={2}>
+                    <Photo height="200px" width="200px"/>
+                </Col>
+                <UserDetails xs={6}>
+                    <h3>Rafael Nunes</h3>
+                    <ul>
+                        <li>Age: 29</li>
+                        <li>Location: Alverca do Ribatejo</li>
+                        <li>Occupation: Frontend Developer</li>
+                    </ul>
+                </UserDetails>
+                <UserDetails xs={4}>
+                    <p>About me:</p>
+                    <p>
+                        My name is Rafael Nunes and I am a Frontend Developer
+                        with solid experience in Typescript and React framework.
+                    </p>
+                </UserDetails>
+            </Row>
+        </UserContainer>
+    )
+}
+
+export default User
---
+++ src/components/user.style.ts
@@ -0,0 +1,33 @@
+import { Col, Grid } from "react-flexbox-grid";
+import styled from "styled-components";
+
+export const UserContainer = styled(Grid)`
+    width: 100%;
+    padding: 0 10rem;
+    margin: 3rem 0;
+    > .row {
+        align-items: center;
+    }
+
+`
+
+export const UserDetails = styled(Col)`
+
+    text-align: left;
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+    
+    > h3 {
+        font-size: 2.25rem;
+        line-height: 2.5rem;
+    }
+
+    > ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    > p {
+        text-align: right;
+    }
+`
