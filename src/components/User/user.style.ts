8ac6a5491e85a2330b48541cea5a3a45dab6a91a
Rafael_LN <39883801+Rafael-LN@users.noreply.github.com>
2021-09-09 11:08:50 +0100

    Prettier fixes

--- src/components/User/user.style.ts
+++ src/components/User/user.style.ts
@@ -1,22 +1,21 @@
-import styled from "styled-components";
-import { Col, Grid } from "react-flexbox-grid";
+import styled from 'styled-components'
+import { Col, Grid } from 'react-flexbox-grid'
 
 export const UserContainer = styled(Grid)`
     width: 100%;
     padding: 0 10rem;
     margin: 3rem 0;
-    
+
     > .row {
         align-items: center;
     }
 `
 
 export const UserDetails = styled(Col)`
-
     text-align: left;
     font-size: 1.125rem;
     line-height: 1.75rem;
-    
+
     > h3 {
         font-size: 2.25rem;
         line-height: 2.5rem;
