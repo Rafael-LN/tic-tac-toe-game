40e56f4a02c74c0b8fa3073f5c7af0ed7e53ddcb
Rafael_LN <39883801+Rafael-LN@users.noreply.github.com>
2021-09-09 11:09:23 +0100

    Created tic tac toe model and default values

--- src/components/TicTacToe/model/tic-tac-toe.default.ts
+++ src/components/TicTacToe/model/tic-tac-toe.default.ts
@@ -1,32 +1,54 @@
-import { BoardCell, PlayerContainers, Score } from "./tic-tac-toe.model";
+import {
+    BoardCell,
+    BoardValues,
+    CellColor,
+    PlayerMoves,
+    Score,
+} from './tic-tac-toe.model'
 
-export const maxMatches = 9;
+export const maxMatches = 9
 
-export const size = 3;
+export const size = 3
 
-export const firstPlayer = BoardCell.X;
+export const firstPlayer = BoardValues.X
 
 export const defaultScore: Score = {
-  [BoardCell.X]: 0,
-  [BoardCell.O]: 0,
-};
+    [BoardValues.X]: 0,
+    [BoardValues.O]: 0,
+}
 
 export const players = {
-  [BoardCell.X]: 1,
-  [BoardCell.O]: 2,
-};
+    [BoardValues.X]: 1,
+    [BoardValues.O]: 2,
+}
 
-export const containers: PlayerContainers = {
+export const initialMoves = (): PlayerMoves => ({
     O: {
-        rows: [],
-        columns: [],
-        diagonal: [],
-        oppositeDiagonal: []
+        rows: Array<number>(size).fill(0),
+        cols: Array<number>(size).fill(0),
+        rightDiagonal: Array<number>(size).fill(0),
+        leftDiagonal: Array<number>(size).fill(0),
     },
     X: {
-        rows: [],
-        columns: [],
-        diagonal: [],
-        oppositeDiagonal: []
-    }
+        rows: Array<number>(size).fill(0),
+        cols: Array<number>(size).fill(0),
+        rightDiagonal: Array<number>(size).fill(0),
+        leftDiagonal: Array<number>(size).fill(0),
+    },
+    E: {
+        rows: Array<number>(size).fill(0),
+        cols: Array<number>(size).fill(0),
+        rightDiagonal: Array<number>(size).fill(0),
+        leftDiagonal: Array<number>(size).fill(0),
+    },
+})
+
+const defaultBoardCell: BoardCell = {
+    value: BoardValues.EMPTY,
+    color: CellColor.EMPTY,
 }
+
+export const initialBoard = (): Array<Array<BoardCell>> =>
+    Array<BoardCell>(size)
+        .fill(defaultBoardCell)
+        .map(() => Array<BoardCell>(size).fill(defaultBoardCell))
--- src/components/TicTacToe/model/tic-tac-toe.model.ts
+++ src/components/TicTacToe/model/tic-tac-toe.model.ts
@@ -1,53 +1,69 @@
-export type Board = Array<Array<BoardCell>>;
+export type Board = Array<Array<BoardCell>>
 
-export enum BoardCell {
+export enum CellColor {
+    DARK = 'dark',
+    BRIGHT = 'bright',
+    EMPTY = 'empty',
+}
+
+export interface BoardCell {
+    value: BoardValues
+    color: CellColor
+}
+
+export enum BoardValues {
     X = 'X',
     O = 'O',
-    EMPTY = ''
+    EMPTY = 'E',
 }
 
 export enum GameStatus {
     START = 'start',
     WIN = 'win',
     DRAW = 'draw',
-    END = 'end'
+    END = 'end',
 }
 
 export interface Cell {
     row: number
-    column: number;
+    col: number
 }
 
 export interface Score {
-    [BoardCell.O]: number;
-    [BoardCell.X]: number;
+    [BoardValues.O]: number
+    [BoardValues.X]: number
 }
 
-export interface Containers {
-    rows: number[];
-    columns: number[];
-    diagonal: number[];
-    oppositeDiagonal: number[];
+export interface Moves {
+    rows: Array<number>
+    cols: Array<number>
+    rightDiagonal: Array<number>
+    leftDiagonal: Array<number>
 }
 
-export interface PlayerContainers {
-    [BoardCell.X]: Containers;
-    [BoardCell.O]: Containers;
+export interface PlayerMoves {
+    [BoardValues.X]: Moves
+    [BoardValues.O]: Moves
+    [BoardValues.EMPTY]: Moves
 }
 
 export interface BoardProps {
-    board: Board;
-    setBoard: (board: Board) => void;
-    status: GameStatus;
-    setStatus: (status: GameStatus) => void;
-    score: Score;
-    setScore: (score: Score) => void; 
-    history: Array<string>;
-    setHistory: (score: Array<string>) => void; 
-    player: BoardCell;
-    setPlayer: (player: BoardCell) => void;
-    playerContainers: PlayerContainers;
-    setPlayerContainers: (playerContainers: PlayerContainers) => void;
-    winnerCells: Array<Cell>;
-    setWinnerCells: (winnerCells: Array<Cell>) => void;
+    board: Board
+    setBoard: (board: Board) => void
+    status: GameStatus
+    setStatus: (status: GameStatus) => void
+    score: Score
+    setScore: (score: Score) => void
+    history: Array<string>
+    setHistory: (score: Array<string>) => void
+    player: BoardValues
+    setPlayer: (player: BoardValues) => void
+    playerMoves: PlayerMoves
+    setPlayerMoves: (playerMoves: PlayerMoves) => void
+    winnerCells: Array<Cell>
+    setWinnerCells: (winnerCells: Array<Cell>) => void
+    resetBoard: () => void
+    isDraw: boolean
+    isWinner: () => boolean
+    endGame: () => void
 }
